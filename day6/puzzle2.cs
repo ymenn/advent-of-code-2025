e38a8a88c7e3291b4a54441183e230890c2a3ad7
menn <ymenn.games@gmail.com>
2025-12-08 21:13:14 +0000

    Day 6 complete

--- day6/puzzle2.cs
+++ day6/puzzle2.cs
@@ -1,7 +1,7 @@
 List<string> lines = [];
 try
 {
-    lines = File.ReadLines("test.txt").ToList();
+    lines = File.ReadLines("input.txt").ToList();
 }
 catch (Exception e)
 {
@@ -17,34 +17,93 @@ lines.RemoveAt(lines.Count - 1);
 
 List<List<string>> operationNumberStrings = [];
 
-foreach (var line in lines)
+var splitLines = SplitLinesByOperation(lines);
+
+foreach (var splitLine in splitLines)
 {
-    string[] splitLine = line.Split(" ", StringSplitOptions.RemoveEmptyEntries); // Need to split only on spaces between numbers not those padding the string
     for (int i = 0; i < splitLine.Count(); i++)
     {
         if (i >= operationNumberStrings.Count)
+        {
             operationNumberStrings.Add([]);
+        }
 
         var numChars = splitLine[i].ToArray();
 
         for (int j = 0; j < numChars.Count(); j++)
         {
-            if (operationNumberStrings[i].Count >= j)
+            if (j >= operationNumberStrings[i].Count)
+            {
                 operationNumberStrings[i].Add("");
+            }
 
-            operationNumberStrings[i][j] += numChars[j];
+            if (!numChars[j].Equals(' '))
+                operationNumberStrings[i][j] += numChars[j];
         }
     }
 }
 
-foreach (var operation in operationNumberStrings)
+for (int i = 0; i < operations.Count(); i++)
 {
-    foreach (var stringNum in operation)
+    var operation = operations[i];
+    long operationResult;
+    if (operation.Equals("*"))
     {
-        Console.Write(stringNum + ",");
+        operationResult = 1L;
+        foreach (var numberString in operationNumberStrings[i])
+        {
+            if (!long.TryParse(numberString, out var number))
+                throw new Exception("Invalid number string");
+            operationResult *= number;
+        }
     }
-    Console.WriteLine();
+    else
+    {
+        operationResult = 0L;
+        foreach (var numberString in operationNumberStrings[i])
+        {
+            if (!long.TryParse(numberString, out var number))
+                throw new Exception("Invalid number string");
+            operationResult += number;
+        }
+    }
+
+    sum += operationResult;
 }
+
 Console.WriteLine(sum);
 
-static List<string> SplitStringKeepPadding(string line) { }
+static List<List<string>> SplitLinesByOperation(List<string> lines)
+{
+    List<List<string>> result = [];
+    for (int x = 0; x < lines.Count(); x++)
+        result.Add([]);
+
+    int currentStartInd = 0;
+    for (int i = 0; i < lines[0].Count(); i++)
+    {
+        var allEmpty = true;
+        foreach (var line in lines)
+        {
+            if (!line[i].Equals(' '))
+            {
+                allEmpty = false;
+                break;
+            }
+        }
+        if (!allEmpty)
+            continue;
+
+        for (int z = 0; z < lines.Count(); z++)
+        {
+            result[z].Add(lines[z].Substring(currentStartInd, i - currentStartInd));
+        }
+        currentStartInd = i + 1;
+    }
+    for (int z = 0; z < lines.Count(); z++)
+    {
+        result[z].Add(lines[z].Substring(currentStartInd, lines[z].Count() - currentStartInd));
+    }
+
+    return result;
+}
